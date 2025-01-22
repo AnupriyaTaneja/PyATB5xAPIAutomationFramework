962a59bcb29d7366f4b3478b22ac8e5a9a205f34
AnupriyaTaneja <anupriya.taneja@gmail.com>
2025-01-22 14:37:13 +0530

    jan2_api framework project

--- src/constants/api_constants.py
+++ src/constants/api_constants.py
@@ -6,6 +6,7 @@ class APIConstants(object):
     def base_url(self):
         return "https://restful-booker.herokuapp.com"
 
+
     def url_create_booking(self):
         return "https://restful-booker.herokuapp.com/booking"
 
